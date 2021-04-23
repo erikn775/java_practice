5d6ba702d9ad61d50b51cc585ed2f68affc40227
erikn775 <erikn775@gmail.com>
2021-04-23 11:44:57 -0600

    added error handling

--- src/TacoCalculator.java
+++ src/TacoCalculator.java
@@ -2,12 +2,21 @@ import javax.swing.JOptionPane;
 
 public class TacoCalculator {
     public static void main(String[] args) {
-        String name, tacoAmount;
+        String name, tacoAmount = "0";
+        boolean isNumber = false;
         double tacoPrice = 2.50;
-        double total;
+        double total = 0;
         name = JOptionPane.showInputDialog("Hi, what is your name?");
-        tacoAmount = JOptionPane.showInputDialog("Hi, " + name + " how many tacos would you like?");
-        total = Integer.parseInt(tacoAmount) * tacoPrice;
+
+        while (total == 0){
+            tacoAmount = JOptionPane.showInputDialog("Hi, " + name + " how many tacos would you like?");
+            try {
+                total = Integer.parseInt(tacoAmount) * tacoPrice;
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Please give amount as a number");
+            }
+        }
+
         JOptionPane.showMessageDialog(null,"Thanks " + name + ", " + tacoAmount + " tacos will be $" + String.format("%.2f", total));
 
     }
