eff4fea14621db0dc8e1f53a318630cbd5725d31
erikn775 <erikn775@gmail.com>
2021-04-23 10:21:08 -0600

    deleted commented code

--- src/TacoCalculator.java
+++ src/TacoCalculator.java
@@ -1,16 +1,3 @@
-//import java.util.Scanner;
-//import javax.swing.JOptionPane;
-//
-//public class MyFirstProgram {
-//    public static void main(String[] args) {
-//        String strAmount;
-//        int intAmount;
-//        String food = "Tacos";
-//        strAmount = JOptionPane.showInputDialog("How many " + food + " would you like?");
-//        intAmount = Integer.parseInt(strAmount);
-//        System.out.println("Ok " + intAmount + " " + food + " coming right up");
-//    }
-//}
 import javax.swing.JOptionPane;
 
 public class TacoCalculator {
