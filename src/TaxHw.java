f002965a1a939408b272707ac44ae29aaab112ab
erikn775 <erikn775@gmail.com>
2021-06-09 20:01:15 -0600

    refactored

--- src/TaxHw.java
+++ src/TaxHw.java
@@ -15,13 +15,9 @@ public class TaxHw {
 
         double totalTaxes = 0;
 
-        int income;
+        String givenIncome = JOptionPane.showInputDialog("What is your gross income?");
 
-        String givenIncome;
-
-        givenIncome = JOptionPane.showInputDialog("What is your gross income?");
-
-        income = Integer.parseInt(givenIncome);
+        int income = Integer.parseInt(givenIncome);
 
         switch (getTaxBracket(income))
         {
@@ -39,21 +35,21 @@ public class TaxHw {
                 totalTaxes += (Math.min(income, 40126) - 9876) * .12;
         }
 
-        JOptionPane.showMessageDialog(null, "You owe $" + totalTaxes + " in taxes");
+        JOptionPane.showMessageDialog(null, "You owe $" + String.format("%.2f", totalTaxes) + " in taxes");
     }
 
     private static TaxBracket getTaxBracket(int grossIncome){
         if(grossIncome <= 9875)
             return TaxBracket.Bracket0;
-        if(grossIncome > 9875 && grossIncome <= 40125)
+        if(grossIncome <= 40125)
             return TaxBracket.Bracket12;
-        if(grossIncome > 40125 && grossIncome <= 85525)
+        if(grossIncome <= 85525)
             return TaxBracket.Bracket22;
-        if(grossIncome > 85525 && grossIncome <= 163300)
+        if(grossIncome <= 163300)
             return TaxBracket.Bracket24;
-        if(grossIncome > 163300 && grossIncome <= 207350)
+        if(grossIncome <= 207350)
             return TaxBracket.Bracket32;
-        if(grossIncome > 207350 && grossIncome <= 518400)
+        if(grossIncome <= 518400)
             return TaxBracket.Bracket35;
 
         return TaxBracket.Bracket37;
